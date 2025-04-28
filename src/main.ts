0bb223529911e5b5a34c465a2611a05eaa773219
pixeldiv <pixelcore84@gmail.com>
2025-04-28 12:18:43 +0300

    chat_ui

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { createApp } from 'vue'
+import { router } from './router';
+import { createPinia } from 'pinia';
+import piniaPluginPersistedState from 'pinia-plugin-persistedstate';
+import './style.css'
+import App from './App.vue'
+
+
+const pinia = createPinia();
+pinia.use(piniaPluginPersistedState);
+
+
+const app = createApp(App);
+app.use(router);
+app.use(pinia);
+app.mount('#app');
